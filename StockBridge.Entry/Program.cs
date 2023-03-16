caadccdbde742367c7ee74e73ab2f81896f35022
onurcan ertekin <onrcn_ertkn@hotmail.com>
2023-03-17 02:00:26 +0300

    move CredentialsDto to global, add login ability to site with credentials

--- StockBridge.Entry/Program.cs
+++ StockBridge.Entry/Program.cs
@@ -1,16 +1,55 @@
 ﻿using CefSharp;
 using CefSharp.OffScreen;
+using StockBridge.Dto;
 using StockBridge.Entry;
 
-//Get configs
-var credentials = ConfigManager.GetCredentials();
-var uri = ConfigManager.GetUri();
-
-//initialize check for cef
-var cefSettings = new CefSettings();
-var initializeCefCheck = Cef.Initialize(cefSettings);
-if (initializeCefCheck == false)
+internal class Program
 {
-    HandleConsole.Exit(false, "Cef couldn't initialized.");
+    private static CredentialsDto credentials = ConfigManager.GetCredentials();
+
+    private static async Task Main(string[] args)
+    {
+        //Get configs
+        SiteUriDto uri = ConfigManager.GetUri();
+
+        //initialize check for cef
+        var cefSettings = new CefSettings();
+        var initializeCefCheck = Cef.Initialize(cefSettings);
+        if (initializeCefCheck == false)
+        {
+            HandleConsole.Exit(false, "Cef couldn't initialized.");
+        }
+        HandleConsole.AddStatus(true, "Cef succesfully initialized.");
+
+        //Create Chromium instance with given uri
+        using (var browser = new ChromiumWebBrowser(uri.SiteUri))
+        {
+            //Try to reach site
+            var initialLoadResponse = await browser.WaitForInitialLoadAsync();
+            if (initialLoadResponse.Success == false)
+            {
+                HandleConsole.Exit(false, "Couldn't reach to site");
+            }
+            HandleConsole.AddStatus(true, "Site initial load is successfull");
+
+            await LoginToSite(browser);
+        }
+    }
+
+    private static async Task LoginToSite(ChromiumWebBrowser browser)
+    {
+        var topbarMenuButtonElement = await browser.EvaluateScriptAsync("document.getElementsByClassName(\"nav-user-menu-button\")[0].click()");
+        HandleConsole.AddStatus(topbarMenuButtonElement.Success, $"On Click Menu Button");
+        //var slotElementInModalPopup = await browser.EvaluateScriptAsync("document.querySelector('cars-global-header').shadowRoot.querySelector('spark-modal').shadowRoot.children[0].getElementsByTagName('slot')[0]");
+        var signInButtonElement = await browser.EvaluateScriptAsync("document.querySelector('cars-global-header').shadowRoot.querySelector('spark-button').click()");
+        HandleConsole.AddStatus(signInButtonElement.Success, $"On Click Sign In Button");
+
+        var addUsernameCredentialToElement = await browser.EvaluateScriptAsync($"document.querySelector('cars-auth-modal').querySelector(\"[id=auth-modal-email]\").value='{credentials.Username}'");
+        HandleConsole.AddStatus(addUsernameCredentialToElement.Success, $"On Add Username to Email Input");
+        var addPasswordCredentialToElement = await browser.EvaluateScriptAsync($"document.querySelector('cars-auth-modal').querySelector(\"[id=auth-modal-current-password]\").value='{credentials.Password}'");
+        HandleConsole.AddStatus(addPasswordCredentialToElement.Success, $"On Add Password to Email Input");
+
+        var signInModalSignInButtonElement = await browser.EvaluateScriptAsync("document.querySelector('cars-auth-modal').shadowRoot.querySelector('spark-button').click()");
+        HandleConsole.AddStatus(signInModalSignInButtonElement.Success, $"On Click Sign In Button In Sign In Modal");
+    }
 }
-HandleConsole.AddStatus(true, "Cef succesfully initialized.");
