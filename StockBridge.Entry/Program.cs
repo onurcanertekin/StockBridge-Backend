811b21072c85ac3c8050c243d4346fe2e05117d7
onurcan ertekin <onrcn_ertkn@hotmail.com>
2023-03-17 00:22:10 +0300

    fetch data from config, write them into record

--- StockBridge.Entry/Program.cs
+++ StockBridge.Entry/Program.cs
@@ -1,1 +1,11 @@
-﻿return;
+﻿using StockBridge.Dto;
+using System.Configuration;
+
+//Get Username and Password from config to auth in cars.com
+string username = ConfigurationManager.AppSettings["username"];
+string password = ConfigurationManager.AppSettings["password"];
+
+//Initialize new record for auth purposes
+var credentials = new CredentialsDto(username, password);
+
+return;
